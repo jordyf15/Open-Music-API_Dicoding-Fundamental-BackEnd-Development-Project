61446e424c685c4ae8fffc3c2a49fd4ae4454d13
jordyf15 <jordyferdian88@gmail.com>
2021-06-25 08:52:04 +0700

    create music table migration

---
+++ migrations/1624585011734_create-music-table.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('musics', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+    },
+    duration: {
+      type: 'INT',
+    },
+    inserted_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('musics');
+};
