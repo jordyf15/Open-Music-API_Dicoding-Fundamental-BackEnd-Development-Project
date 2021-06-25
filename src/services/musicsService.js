9cc99ed42661b12a45bf3167c1ff6b2342f75c16
jordyf15 <jordyferdian88@gmail.com>
2021-06-25 09:04:27 +0700

    created music service and addmusic

---
+++ src/services/musicsService.js
@@ -0,0 +1,31 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+
+class MusicsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addMusic({
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+  }) {
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const query = {
+      text: 'INSERT INTO musics VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
+      values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows[0].id;
+  }
+}
+
+module.exports = MusicsService;
