61643aefd115137051300f1a4f9c1281df0ae6c2
jordyf15 <jordyferdian88@gmail.com>
2021-07-02 18:38:26 +0700

    created register user functionality

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
 const songs = require('./api/songs');
 const SongsService = require('./services/songsService');
 const SongsValidator = require('./validator/songs');
+
+const users = require('./api/users');
+const UsersService = require('./services/usersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const server = Hapi.server({
@@ -17,14 +22,24 @@ const init = async () => {
   });
 
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
