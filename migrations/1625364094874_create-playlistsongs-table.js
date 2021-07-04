61c7565dc388b1ea8b9f65c09d92b03dcf058c1d
jordyf15 <jordyferdian88@gmail.com>
2021-07-04 14:07:17 +0700

    fixed migrate down for create playlistsongs table and add unique constraint

--- migrations/1625364094874_create-playlistsongs-table.js
+++ migrations/1625364094874_create-playlistsongs-table.js
@@ -18,10 +18,11 @@ exports.up = (pgm) => {
     },
   });
 
+  pgm.addConstraint('playlistsongs', 'unique_playlist_id_and_song_id', 'UNIQUE(song_id, playlist_id)');
   pgm.addConstraint('playlistsongs', 'fk_playlistsongs.playlist_id_playlists.id', 'FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
   pgm.addConstraint('playlistsongs', 'fk_playlistsongs.song_id_songs.id', 'FOREIGN KEY (song_id) REFERENCES songs(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
-  pgm.deleteTable('playlistsongs');
+  pgm.dropTable('playlistsongs');
 };
